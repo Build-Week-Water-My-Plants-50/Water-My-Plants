6866240313c84823bd70829ddd858650ee7e4aa7
kennedybryant <kennedy_marisa96@yahoo.com>
2021-10-21 13:45:31 -0400

    Some Styling on Plant.js

--- water-my/src/components/Plant.js
+++ water-my/src/components/Plant.js
@@ -1,26 +1,20 @@
 import React, {useEffect} from "react";
 import axiosWithAuth from "./axiosWithAuth";
 import { useHistory } from "react-router-dom";
+import "./Plant.css";
 
 export default function Plant(props) {
     const { push } = useHistory();
     const { plant, plants, set_plant_values} = props;
 
-
-
-
     const delete_plant = () => {
-
         axiosWithAuth().delete(`https://watermyplantsweb46.herokuapp.com/api/plants/${plant.plants_id}`, plant)
             .then(res => {
-                console.log(res);
-              push('/PlantsList');
+                set_plant_values([res.data])
+                push('/PlantsList');
             })
-            .catch(err => {
-                console.error(err)
-            })
+            
     }
-
 
     const edit_plant = () => {
         console.log(plant)
@@ -48,15 +42,15 @@ export default function Plant(props) {
 
     return (
         <>
-            <div className="plant-card">
+            <form className="plant-card">
                 <h3>Plant Info</h3>
                 <p>Nickname: {plant.nickname}</p>
                 <p>Species: {plant.species}</p>
                 <p>Water frequency? {plant.h2oFrequency}</p>
                 <img src={`${plant.image}`} alt={`Image of a ${plant.nickname}.`}/>
-                <button onChange={update_form}>Edit Plant</button>
-                <button onClick={delete_plant}>Delete Plant</button>
-            </div>
+                <button id="edit-button" onChange={update_form}>Edit Plant</button>
+                <button id ="delete-button"onClick={delete_plant}>Delete Plant</button>
+            </form>
 
         </>
     )
